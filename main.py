d63a860a89d721dea97042d9a0adf7c70c25ff8e
Taichi Uno <taichi@Taichis-MacBook-Pro.local>
2024-02-19 22:38:35 +0100

    Run LLM locally experiment

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import llm
-import google.generativeai as genai
-
-def get_api_key(key_name):
-    file_path = "api.txt"
-    api_keys = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            key, value = line.strip().split('=')
-            api_keys[key.strip()] = value.strip()
-
-    return api_keys[key_name]
-
-def prompt_chatGPT(text):
-    model = llm.get_model("gpt-3.5-turbo")
-    model.key = get_api_key("gpt-key")
-    response = model.prompt(text)
-    return response.text()
+# import google.generativeai as genai
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
 
 
-def prompt_googleai(text):
-    genai.configure(api_key=get_api_key("google-ai-key"))
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(text)
-    return response.text
+# https://huggingface.co/docs/transformers/main/en/installation
 
+save_path = "./models/t5-small"
 
+def save_model():
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    tokenizer.save_pretrained(save_path)
+    model.save_pretrained(save_path)
 
+def load_model():
+    tokenizer1 = T5Tokenizer.from_pretrained(save_path)
+    model1 = T5ForConditionalGeneration.from_pretrained(save_path)
+    input_id = tokenizer1(["translate English to German: The house is wonderful."], return_tensors="pt").input_ids
+    output = model1.generate(input_id)
+    output = tokenizer1.decode(output[0], skip_special_tokens=True)
+    return output
 
+print(load_model())
+# hf_hub_download(repo_id="bigscience/T0_3B", filename="config.json", cache_dir=save_path)
