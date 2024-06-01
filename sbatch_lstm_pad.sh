3a0c46e39c9f7a507248ec2756c1f0e3bbe56b6f
Taichi Uno <taichi@Taichis-MacBook-Pro.local>
2024-06-01 17:53:49 +0200

    update sbatch

--- sbatch_lstm_pad.sh
+++ sbatch_lstm_pad.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=medium         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
 #SBATCH --time=06:00:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
-#SBATCH --cpus-per-task=2   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --cpus-per-task=1   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
 #SBATCH --mem= 15360   # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
 #SBATCH --mail-type=ALL     # Set mail type to 'END' to receive a mail when the job finishes. 
 #SBATCH --output=slurm_%j.out # Set name of output log. %j is the Slurm jobId
