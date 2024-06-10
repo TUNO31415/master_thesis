be6d5514cba36becd096df381cd6cf5f2e04449d
Taichi Uno <taichi@Taichis-MacBook-Pro.local>
2024-06-10 12:34:27 +0200

    test

--- prompt_creation.py
+++ prompt_creation.py
@@ -153,9 +153,10 @@ def llm_input_generator_with_context(df, speaker00_name, speaker01_name, batch_i
     col_df = pd.read_csv("/tudelft.net/staff-umbrella/tunoMSc2023/paco_dataset/Coordination/curated_coord_data_ffinal.csv")
     print(col_df.head())
     print(batch_id)
-    col_filtered_df = col_df[col_df['batchID'] == batch_id]
+    col_filtered_df = col_df[col_df["batchID"] == batch_id]
     print(col_df.head())
-    taskType = col_filtered_df['coord_taskType'].tolist()[0]
+    print(col_filtered_df["coord_taskType"])
+    taskType = col_filtered_df["coord_taskType"].tolist()
     print(taskType)
 
     
